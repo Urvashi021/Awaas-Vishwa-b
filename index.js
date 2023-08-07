6e0df276b44c2e5ca69ac0ecc2f6de22c83b269f
Urvashi Parmar <parmarurvashi01@gmail.com>
2023-08-07 19:05:27 +0530

    feat : User Register.

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 //console.log(process.env.DATABASE_URL)
 //console.log(process.env.SERVER_PORT)
 
-require ('dotenv').config()
-
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const healthRoute = require('./routes/HealthRoute')
 const authRoutes = require('./routes/AuthRoutes')
 
-//APP
-const app = express()
-app.use(express.json())
 
-//SERVER LISTEN
-app.listen(process.env.SERVER_PORT, ()=> console.log("Our Server is Running at 3000 port."))
+/**
+ * APP
+ */
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-//DATABASE CONNECTION
-mongoose.connect(process.env.DATABASE_URL)
 
-mongoose.connection.once('connected', ()=> console.log('Database Connected.'))
+/**
+ * DATABASE CONNECTION
+ */
+mongoose.connect(process.env.DATABASE_URL);
+mongoose.connection.once('connected', () => console.log('Database connected'));
+mongoose.connection.on('error', (er) => console.log("Database error :", er))
 
-mongoose.connection.on('error', (er)=> console.log('Database Error :', er))
 
-//ROUTE
+/**
+ * ROUTES
+ */
 app.use('/health', healthRoute)
-
 app.use('/api/v1/auth', authRoutes)
 
+
+/**
+ * SERVER LISTEN
+ */
+app.listen(process.env.SERVER_PORT, () => console.log(`App server started at ${process.env.SERVER_PORT}`) );
