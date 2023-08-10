57e21e60bc6edc49f47d76ab43dd762bf40e8d16
Urvashi Parmar <parmarurvashi01@gmail.com>
2023-08-10 22:00:46 +0530

    feat : Login check

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const healthRoute = require('./routes/HealthRoute')
 const authRoutes = require('./routes/AuthRoutes')
 
@@ -12,21 +13,15 @@ const authRoutes = require('./routes/AuthRoutes')
 /**
  * APP
  */
-var whitelist = ['http://localhost:5173', /** other domains if any */ ]
-var corsOptions = {
-  credentials: true,
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+
 
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(cors({
+  credentials: true,
+  origin: ['https://awaas-vishwa-project.vercel.app'],
+}));
 
 
 /**
